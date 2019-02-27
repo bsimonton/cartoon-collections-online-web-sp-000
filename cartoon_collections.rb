d6d804c4b2776499b028fbaf307cd0e65eeb1978
Brian Simonton <bsimonton78@gmail.com>
2019-02-27 13:02:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,9 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  def find_the_cheese(array)
+  
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
-end
 
 end
