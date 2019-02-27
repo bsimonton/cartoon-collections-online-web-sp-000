ecf42066008aeb3edef52b89dec4ff3841c706ba
Brian Simonton <bsimonton78@gmail.com>
2019-02-27 17:06:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,17 @@ def roll_call_dwarves(array)
   
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  
+  
   
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
+
+
 
 def find_the_cheese(array) 
   
